a2dbf7cb85e9cb0e65d5f898fe83aa7f485e0b8d
RheaHewett <rhewett@sbacademy.org>
2016-12-07 15:31:16 -0800

    Create warmup.rb

---
+++ warmup.rb
@@ -0,0 +1,13 @@
+favPetNames= ["Taji", "Bella", "Musa", "Thor", "Koa"]
+
+favPetNames.each do |names|
+  puts names
+end
+
+or 
+
+for names in favPetNames
+  puts names
+end
+
+#I like the .each loop better because it is easier to understand and write.
